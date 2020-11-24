f132e7c0ba6cb3b5d475d73183fae1c6b455530b
Alessandro Vernassa <alessandrovernassa@brainvalley.it>
2020-11-24 09:07:03 +0100

    update artificials

---
+++ misc/fndatabase/ctl_walls.php
@@ -0,0 +1,17 @@
+<?php exit(0);?>
+<tables>
+    <field>
+        <name>id</name>
+        <type>string</type>
+        <primarykey>1</primarykey>
+        <extra>autoincrement</extra>
+        <frm_show>0</frm_show>
+    </field>
+    <field>
+        <name>walls</name>
+        <frm_it>Pareti</frm_it>
+        <frm_en>Walls</frm_en>
+        <type>string</type>
+    </field>
+    <driver>mysql</driver>
+</tables>
