6aec31d31b31ccae14b18cd38308020007cc35a3
speleoalex <speleoalex@gmail.com>
2025-08-26 17:14:04 +0200

    update

---
+++ loadfinis.php
@@ -0,0 +1,6 @@
+<?php
+header('X-Accel-Buffering: no');
+ini_set("display_errors","on");
+error_reporting(E_ALL);
+require_once "./finis/src/include/finis.php";
+require_once "./finis/src/FINIS.php";
---
+++ sections/map_glacial/default.xml.php
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<?php exit(0);?>
+<fn_sections>
+	<type></type>
+	<parent>speleologia_glaciale</parent>
+	<position>18</position>
+	<title>Mappa delle cavit&agrave; glaciali</title>
+	<description></description>
+	<startdate></startdate>
+	<enddate></enddate>
+	<status>1</status>
+	<hidden></hidden>
+	<accesskey></accesskey>
+	<keywords></keywords>
+	<sectionpath>sections</sectionpath>
+	<level></level>
+	<group_view></group_view>
+	<group_edit>w_artificials,w_caves</group_edit>
+	<blocksmode></blocksmode>
+	<blocks></blocks>
+	<title_it>Mappa delle cavit&agrave; glaciali</title_it>
+	<description_it></description_it>
+	<title_en>Map of glacial caves</title_en>
+	<description_en></description_en>
+	<title_fr>Carte des cavit&eacute;s glaciaires</title_fr>
+	<description_fr></description_fr>
+</fn_sections>
